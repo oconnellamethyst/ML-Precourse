6144915db6d649a48def67fd82a6c26490a97bd6
Amethyst <oconnellamethyst@gmail.com>
2018-03-14 21:20:17 -0500

    Calculus Portion

--- precourse.py
+++ precourse.py
@@ -11,3 +11,67 @@
 def f(x):
     return x**2
 
+# Amethyst OConnell
+
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return x**3 + 5*x
+
+def d_f(x):
+    return 2*x
+
+def d_f_2(x):
+    return 3*x**2
+
+def d_f_3(x):
+    return 3*x**2
+
+def vector_sum(v):
+    return # something sums vectors
+
+def vector_less(v):
+    return # something subtracts vectors
+
+def vector_magnitude(v):
+    return # something gets the length of vectors
+
+def vec5(v):
+    return # something
+'''
+Using numpy array
+[1
+ 1
+ 1
+ 1
+ 1]
+'''
+
+def vec3(v):
+    return # something
+'''
+Using numpy array
+[0
+ 0
+ 0]
+'''
+
+def vec2_1(v):
+    return # something
+'''
+Using numpy array
+[1
+ 0]
+'''
+
+def vec2_2(v):
+    return # something
+'''
+Using numpy array
+[0
+ 1]
+'''
+
+def matrix_multiply(vec,matrix):
+    return # something, multiplies 2x2 matrix
